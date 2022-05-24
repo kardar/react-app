97930f4fe2c1ea0bec029ef6de3a40c94e5a48a1
haideratish2689@gmail.com <haideratish2689@gmail.com>
2022-05-24 21:23:12 +0500

    Search bar and dropdown menu styled

--- src/components/LeftNavBar.js
+++ src/components/LeftNavBar.js
@@ -5,15 +5,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const LeftNavBar = () => {
   return (
     <div className="leftNav-container">
-    <nav className="leftnav-bar">
-      <h1 ><button className="btn-departments">All Departments</button></h1>
-      <ul>
-        <li>Fruit</li>
-        <li>Fruit</li>
-        <li>Fruit</li>
-        <li>Fruit</li>
-      </ul>
-      </nav>
+     <div className="dropdown">
+       <button className="dropbtn">CatogriesCatogries    </button>
+       <div className="dropdown-content">
+       <a href="#">Vegatables</a>
+       <a href="#">Meat</a>
+      <a href="#">Fruits</a>
+      <a href="#">Groceries</a>
+       </div>
+     </div>
       <div className="search-container">
       <input className="search-bar" type="text" placeholder="What do you need?"></input>
       <button className="search-btn" type="submit">Search</button>
