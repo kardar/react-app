b620820a6142243fd237d28cf7339214fa2d4d04
haideratish2689@gmail.com <haideratish2689@gmail.com>
2022-05-24 16:16:13 +0500

    side menu updated

--- src/components/LeftNavBar.js
+++ src/components/LeftNavBar.js
@@ -1,17 +1,24 @@
 import React from "react";
+import '../style/leftNavBar.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const LeftNavBar = () => {
   return (
-    <nav>
-      <h1>Left NAVIGATION BAR</h1>
+    <div className="leftNav-container">
+    <nav className="leftnav-bar">
+      <h1 ><button className="btn-departments">All Departments</button></h1>
       <ul>
         <li>Fruit</li>
         <li>Fruit</li>
         <li>Fruit</li>
         <li>Fruit</li>
       </ul>
-    </nav>
+      </nav>
+      <div className="search-container">
+      <input className="search-bar" type="text" placeholder="What do you need?"></input>
+      <button className="search-btn" type="submit">Search</button>
+      </div>
+    </div>
   );
 };
 export default LeftNavBar;
